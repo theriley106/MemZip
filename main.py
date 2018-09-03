c6800f3b0c2733824fdc26a210e145a15d29f04e
Theriley106 <christopherlambert106@gmail.com>
2018-09-03 13:46:40 -0700

    Added gen card

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import requests
 import json
 
 URL_DB = json.load(open("urls.json"))
-
+# Gets the urls
+ALL_CARDS = json.load(open("all_cards.json"))
+# Gets the list of all cards
+CARD_DB = {}
+for val in ALL_CARDS:
+	CARD_DB[val['id_val']] = val
+# Creates a map of card values
 HIGH_SCORE_URL = URL_DB["highscore"]
+
+
 
 def convert_message(message):
 	# Converts the message into a python dict
 	return json.loads(message)
+
+def get_card(id_val):
+	return CARD_DB[id_val]
 
 def get_high_scores():
 	# Returns a python dictionary containing high scores
@@ -37,5 +48,6 @@ class game(object):
 
 
 if __name__ == '__main__':
-	print start_new_game('test')
+	#print start_new_game('test')
 	#print get_high_scores()
+	print get_card(31)['image']
