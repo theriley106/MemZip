1ff4f2dca6a64ac5c307cb95653dbfeeda6ca5b3
Theriley106 <christopherlambert106@gmail.com>
2018-09-03 15:48:10 -0700

    Added body of card

--- card.py
+++ card.py
@@ -13,6 +13,19 @@ def create_line1(value, width):
 	string += right_string
 	return string
 
+def create_line2(value, width):
+	right_string = "│"
+	left_string = "│"
+	string = left_string
+	string += value
+	if value == '10':
+		string += ""
+	else:
+		string += " "
+	string += " " * (width-4)
+	string += right_string
+	return string
+
 def create_cards(height, width, cards):
 	corner_left_string = "┌"
 	corner_right_string = "┐"
@@ -23,9 +36,14 @@ def create_cards(height, width, cards):
 	for card in cards:
 		card_template[0].append(corner_left_string + ''.join([top_string * (width-2)]) + corner_right_string)
 		card_template[1].append(create_line1(card['value'], width))
+		for i in range((height-5)/2):
+			string = "│"
+			string += " " * (width-2)
+			string += "│"
+			card_template[2+i].append(string)
 		for va in card_template:
 			print str(va[0])
 
 if __name__ == '__main__':
-	create_cards(15, 15, [{"value": "10"}])
+	create_cards(10, 15, [{"value": "10"}])
 
